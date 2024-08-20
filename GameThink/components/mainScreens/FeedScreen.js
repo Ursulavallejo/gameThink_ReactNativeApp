6536c58aad7df4aac7ae8a92da543ced5d3bc2c9
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-08-20 12:56:54 +0200

    updated user json with favorite game/ descripcion

--- GameThink/components/mainScreens/FeedScreen.js
+++ GameThink/components/mainScreens/FeedScreen.js
@@ -1,11 +1,30 @@
 import { Text, View } from 'react-native'
+import { useEffect, useState } from 'react'
 import { globalStyles } from '../../styles/globalStyles'
+// import { Card } from '../Card'
+import { Heading } from '../BasicReusableComponents'
 
 export function FeedScreen() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch(
+      'https://raw.githubusercontent.com/Ursulavallejo/gameThink_ReactNativeApp/main/GameThink/data/users.json'
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setUsers(result)
+      })
+  })
+
   return (
     <View style={globalStyles.layout}>
-      <Text style={globalStyles.title}>We Go Further Together!!</Text>
-      <Text style={globalStyles.title}>Feed!</Text>
+      <Heading>We Go Further Together!!</Heading>
+      {/* <Text style={globalStyles.title}>Feed!</Text> */}
+
+      {/* print all the Api */}
+      <Text>{JSON.stringify(users)}</Text>
+      {/* <Card></Card> */}
     </View>
   )
 }
